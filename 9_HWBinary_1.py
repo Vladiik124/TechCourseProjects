aafb418a8499994dfaaac99c20c03d925672992e
Vladiik124 <114525164+Vladiik124@users.noreply.github.com>
2024-10-04 13:08:27 +0300

    Add files via upload

---
+++ 9_HWBinary_1.py
@@ -0,0 +1,15 @@
+
+def negativeBinary(binary_str):
+    complement = binary_str.replace("0", "2").replace("1", "0").replace("2", "1")
+    negative_value = bin(int(complement, 2) + 1)[2:]
+    return negative_value.zfill(8)
+
+FloatValue = float(input("Enter float value: "))
+FloatValue = int(FloatValue)
+
+binary = bin(FloatValue & 0xFF)[2:].zfill(8)
+
+
+# Print the binary numbers
+print(f"positive binary: {binary}")
+print(f"negative binary: {negativeBinary(binary)}")
