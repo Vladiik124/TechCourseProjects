d10b93663767d1c7432469777f4548c98f651f8f
Vladiik124 <vladimirkotliar8@gmail.com>
2024-11-13 17:09:21 +0000

    virus total added

--- 4_VirusTotal/main.py
+++ 4_VirusTotal/main.py
@@ -1,19 +1,50 @@
+import os
 import requests
+import time
 
 api_key = input("Enter API key: ")
 
-url = "https://www.virustotal.com/api/v3/files"
+folder_path = "FilesToScan"
+upload_url = "https://www.virustotal.com/api/v3/files"
+report_url = "https://www.virustotal.com/api/v3/analyses/"
 
 headers = {
     "accept": "application/json",
-    "content-type": "multipart/form-data",
     "x-apikey": api_key
 }
-file_path = "FilesToScan/skeebi.txt"
 
-with open(file_path, "rb") as file:
-    files = {'file': (file_path, file)}
+for filename in os.listdir(folder_path):
+    file_path = os.path.join(folder_path, filename)
+    
+    if not os.path.isfile(file_path):
+        continue
 
-    response = requests.post(url, headers=headers, files=files)
+    with open(file_path, "rb") as file:
+        files = {'file': (filename, file)}
 
-print(response.text)
+        response = requests.post(upload_url, headers=headers, files=files)
+        
+        if response.status_code == 200:
+            data = response.json()
+            analysis_id = data['data']['id']
+            print(f"Checking {filename} results...")
+
+            while True:
+                report_response = requests.get(report_url + analysis_id, headers=headers)
+                report_data = report_response.json()
+
+                if report_response.status_code == 200 and 'data' in report_data:
+                    status = report_data['data']['attributes']['status']
+                    if status == 'completed':
+                        stats = report_data['data']['attributes']['stats']
+                        print(f"File: {filename}")
+                        print(f"Results: {stats}")
+                        break
+                    else:
+                        print("Analysis in progress")
+                        time.sleep(10)
+                else:
+                    print(f"Failed to report for {filename}. {report_response.status_code}")
+                    break
+        else:
+            print(f"Failed to upload {filename}. {response.status_code}")
